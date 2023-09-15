c6a521a435467e3749b4ea0b0eb94b7373ac65c4
humy06001 <humy06001@E-PW0234KS>
2023-09-15 10:34:24 +0200

    Add project files.

---
+++ Kap 2 Uppgift 2.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Frågar lönen för anställd 1
+            Console.WriteLine("Hur mycket pengar tjänar anställd 1?");
+            int salary1 = int.Parse(Console.ReadLine());
+
+            //Anställd 2 
+            Console.WriteLine("Hur mycket pengar tjänar anställd 2?");
+            int salary2 = int.Parse(Console.ReadLine());
+
+            //Anställd 3
+            Console.WriteLine("Hur mycket pengar tjänar anställd 3?");
+            int salary3 = int.Parse(Console.ReadLine());
+
+            //Medellönen
+            int average = (salary1 + salary2 + salary3)/3;
+            
+            //Skriver ut medellönen
+            Console.WriteLine($"Medellönen för personalen är {average}kr.");
+            Console.ReadKey();
+        }
+    }
+}
